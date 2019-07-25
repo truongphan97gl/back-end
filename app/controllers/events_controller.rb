81e06213c23e8824c82ab07ec0e60c6d5b780583
truongphan97gl <truongphan97gl@gmail.com>
2019-07-25 16:58:45 -0400

    Order the update.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ProtectedController
 
   # GET /events
   def index
-    @events = current_user.events.all
+    @events = current_user.events.all.order('id ASC')
 
     render json: @events
   end
